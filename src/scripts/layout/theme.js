ebffb647eb93446fed6551b421e693a844debfbc
Oskar <oskarradonkimball@gmail.com>
2019-07-15 23:15:57 -0700

    working menu toggle button

--- src/scripts/layout/theme.js
+++ src/scripts/layout/theme.js
@@ -4,12 +4,9 @@ import "../../styles/theme.scss.liquid";
 // make header logo smaller on scroll to keep out of view
 
 $(document).on("scroll", () => {
-		if
-      (($(window).width() < 940) && ($(document).scrollTop() > 50)){
-		      $("header").addClass("shrink");
-		  }
-		else
-		{
+		if (($(window).width() < 940) && ($(document).scrollTop() > 50)){
+		   $("header").addClass("shrink");
+		} else {
 			$("header").removeClass("shrink");
 		}
 	});
@@ -17,18 +14,29 @@ $(document).on("scroll", () => {
 
 // menu button on tiny phones like iphone 3/4
 
-if ($(window).width() < 368) {
-	$("#header-nav").hide();
+headerMenu();
 
-	$("#menu-button").click(() => {
-		if (!$("#header-nav").hasClass('visible')) {
-			$("#header-nav").show();
-			$("#header-nav").addClass('visible');
-			$("#menu-button").text("X");
-		} else {
-			$("#header-nav").hide();
-			$("#header-nav").removeClass('visible');
-			$("#menu-button").text("Menu");
-		}
-	});
+$(window).resize(function(){
+	headerMenu();
+});
+
+function headerMenu() {
+	let windowWidth = $(window).width();
+	if ((windowWidth < 368) && (!$("#header-nav").hasClass('visible'))) {
+		$("#header-nav").hide();
+
+		$("#menu-button").click(() => {
+			if (!$("#header-nav").hasClass('visible')) {
+				$("#header-nav").show();
+				$("#header-nav").addClass('visible');
+				$("#menu-button").text("X");
+			} else {
+				$("#header-nav").hide();
+				$("#header-nav").removeClass('visible');
+				$("#menu-button").text("Menu");
+			}
+		});
+	} else {
+		$("#header-nav").show();
+	}
 }
